0e37ce566b6c3fb926532aad848ca5a44e2896a0
namariaa <anamariafag18@gmail.com>
2026-08-21 06:30:37 -0300

    :sparkles: feat: Adiciona insertion sort. #1

---
+++ ordenacao/insertionSort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> insertionSort(vector<int> numberlist) {
+    for (int i = 1; i < numberlist.size(); i++) {
+        int index = i;
+        int moveNumber = numberlist[index]; 
+        while (moveNumber < numberlist[index - 1]) {
+            numberlist[index] = numberlist[index - 1];
+            index--;
+        }
+        numberlist[index] = moveNumber;
+    }
+    return numberlist;
+}
+
+int main() {
+    vector<int> numberlist;
+    int v;
+    for(int i = 0; i < 5; i++) {
+        cin >> v;
+        numberlist.push_back(v);
+    }
+    vector<int> sortList = insertionSort(numberlist);
+    
+    for (int i = 0; i < sortList.size(); i++) {
+        cout << sortList[i] << endl;
+    }
+
+    return 0;
+}
